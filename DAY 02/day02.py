b3020ab7dba7e8ad42e560339143257ce1221366
antonio.sanko <37519471+tonto98@users.noreply.github.com>
2019-12-02 16:53:17 +0100

    Update day02.py

--- DAY 02/day02.py
+++ DAY 02/day02.py
@@ -3,11 +3,8 @@ lines = f.readlines()
 array = lines[0].strip().split(",")
 originalIpnut = [int(i) for i in array]
 
-
 GOAL = 19690720
 
-
-#before?
 for noun in range(0,100):
 	for verb in range(0,100):
 
@@ -27,49 +24,4 @@ for noun in range(0,100):
 			currIndex += 4
 		if(results[0] == GOAL):
 			print("FINALNO: " + str(100*results[1] + results[2]))
-			break
-
-
-
-print(results)
-print("------------------------")
-print(originalIpnut)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-"""
-f = open("in.txt")
-lines = f.readlines()
-array = lines[0].strip().split(",")
-results = [int(i) for i in array]
-
-#before?
-results[1] = 12
-results[2] = 2
-
-
-currIndex = 0
-
-while (results[currIndex] != 99):
-	if results[currIndex] == 1:
-		#add
-		results[results[currIndex+3]] = results[results[currIndex+1]] + results[results[currIndex+2]]
-
-	elif results[currIndex] == 2:
-		#multiply
-		results[results[currIndex+3]] = results[results[currIndex+1]] * results[results[currIndex+2]]
-	currIndex += 4
-
-print(results)
-"""
+			quit()
